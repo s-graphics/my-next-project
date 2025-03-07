340f041702260b27a75f9b0430c96167e2a59ec4
Sgraphics <info@s-graphics.jp>
2025-03-07 12:19:36 +0900

    第5章まで完了

--- my-next-project/app/_components/Header/index.tsx
+++ my-next-project/app/_components/Header/index.tsx
@@ -1,18 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
+import Menu from "../Menu";
 
 export default function Header(){
     return (
-        <header className={styles.header}>
-        <a href="/" className={styles.logoLink}>
-            <Image src="/logo.svg"
+      <header className={styles.header}>
+        <Link href="/" className={styles.logoLink}>
+          <Image
+            src="/logo.svg"
             alt="SIMPLE"
             className={styles.logo}
             width={348}
             height={133}
             priority
-            />
-        </a>
-        </header>
+          />
+        </Link>
+        <Menu />
+      </header>
     );
 }
