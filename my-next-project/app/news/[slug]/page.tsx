6b76d1ecdde6b3920e2d55a28aebd7c4d65d50af
Sgraphics <info@s-graphics.jp>
2025-03-10 16:43:11 +0900

    第７章まで完了

--- my-next-project/app/news/[slug]/page.tsx
+++ my-next-project/app/news/[slug]/page.tsx
@@ -7,11 +7,16 @@ import styles from "./page.module.css";
 type Props = {
     params:{
         slug:string;
-    }
-}
+    };
+    searchParams:{
+        dk?:string;
+    };
+};
 
-export default async function Page({params}:Props){
-    const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({params,searchParams}:Props){
+    const data = await getNewsDetail(params.slug,{
+        draftKey:searchParams.dk,
+    }).catch(notFound);
 
     return(
         <>
